e44332d04c9544ed976069d90d17f5850aa6458e
Fangorn <dev.dmytro@retrostylegames.com>
2026-08-15 22:15:28 +0300

    :hammer: tips added

--- Assets/Features/LevelHUDModule/Scripts/HUDView.cs
+++ Assets/Features/LevelHUDModule/Scripts/HUDView.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Features.GameContextsModule.Scripts;
 using Features.LevelDesign.Scripts;
+using Features.MainMenuModule.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,14 +9,19 @@ namespace Features.LevelHUDModule.Scripts {
     public class HUDView : MonoBehaviour {
         [SerializeField] private Button _restartButton;
         [SerializeField] private Button _goToMainMenuButton;
+        [SerializeField] private GameObject _tipsRoot;
 
         private ILevelSceneService _levelSceneService;
         private LevelsModel _levelsModel;
-        
+        private LevelsSequenceConfiguration _levelsSequenceConfiguration;
+
         private void OnEnable() {
             _levelSceneService ??= ServiceLocator.Get<ILevelSceneService>();
             _levelsModel ??= ServiceLocator.Get<LevelsModel>();
-            
+            _levelsSequenceConfiguration ??= ServiceLocator.Get<LevelsSequenceConfiguration>();
+
+            UpdateTipsVisibility();
+
             _restartButton.onClick.AddListener(Restart);
             _goToMainMenuButton.onClick.AddListener(GoToMainMenu);
             _levelsModel.OnSceneSwitchStarted += SetButtonsNotInteractable;
@@ -32,6 +39,18 @@ namespace Features.LevelHUDModule.Scripts {
 
         private async void GoToMainMenu() =>
             await _levelSceneService.GoToMainMenu();
+
+        // Tips only make sense while the player is learning, so they live on the first level only.
+        private void UpdateTipsVisibility() {
+            if (_tipsRoot == null)
+                return;
+
+            List<LevelConfiguration> levelsSequence = _levelsSequenceConfiguration.LevelsSequence;
+            bool isFirstLevel = levelsSequence.Count > 0
+                                && levelsSequence[0].LevelBehaviour == _levelsModel.CurrentLevelPrefab;
+
+            _tipsRoot.SetActive(isFirstLevel);
+        }
 
         private void SetButtonsNotInteractable() {
             _restartButton.interactable = false;
